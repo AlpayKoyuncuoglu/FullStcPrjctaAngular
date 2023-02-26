00d691db4eb34cc14d78f4069ae4d8b6f43d65fb
AlpayKoyuncuoglu <alpaykoyuncuoglu1@gmail.com>
2023-02-26 14:55:03 +0300

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fllstck';
+  categoryName = 'Telefon';
+
+  products=[
+    {id:1,name:"samsung s5",price:2000,isActive:true},
+    {id:2,name:"samsung s6",price:3000,isActive:true},
+    {id:3,name:"samsung s7",price:4000,isActive:false},
+    {id:4,name:"samsung s8",price:5000,isActive:false},
+    {id:5,name:"samsung s8",price:6000,isActive:true},
+  ]
+
 }
